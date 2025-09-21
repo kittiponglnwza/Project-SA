87254c0d438868135a605dd62b433b1e1b4c82c0
T.KITTIPONG <kitipongzaza566@gmail.com>
2025-09-21 20:03:48 +0700

    ข้อความ commit

--- font_end/src/main.jsx
+++ font_end/src/main.jsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' //./pages/login_and_register.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
